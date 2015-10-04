ed48ce66e9c589737bcc074264365f5952bd333d
Valeska Medeiros <vahmedeiros@gmail.com>
2015-10-04 12:31:29 -0300

    Metodo de calculo de distancia entre duas coordenadas

--- AppDionisio/AppDionisio/AppDionisio/Models/Coordinates.cs
+++ AppDionisio/AppDionisio/AppDionisio/Models/Coordinates.cs
@@ -31,5 +31,26 @@ namespace AppDionisio.Models
 
             return coordinates;
         }
+
+        //Haversine formula
+        //https://limzhenghong.wordpress.com/2013/11/04/calculate-distance-between-two-locations-with-latitude-and-longitude-c/
+        public double Distance(double Latitude1, double Longitude1, double Latitude2, double Longitude2)
+        {
+            double R = 6371.0;          
+            double dLat = this.toRadian(Latitude2 - Latitude1);
+            double dLon = this.toRadian(Longitude2 - Longitude1);
+
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos(this.toRadian(Latitude1)) * Math.Cos(this.toRadian(Latitude2)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+            double c = 2 * Math.Asin(Math.Min(1, Math.Sqrt(a)));
+            double d = R * c;
+
+            return d;
+        }
+
+        private double toRadian(double val)
+        {
+            return (Math.PI / 180) * val;
+        }
     }
 }
