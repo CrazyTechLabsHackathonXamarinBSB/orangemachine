ba50357e8aedaec329dca05d4ed918d571769442
julio10santos <juliomjk@outlook.com>
2015-10-04 05:08:18 -0300

    Layout tela de detalhes

--- AppDionisio/AppDionisio/AppDionisio/Views/CheckinPage.xaml.cs
+++ AppDionisio/AppDionisio/AppDionisio/Views/CheckinPage.xaml.cs
@@ -13,6 +13,40 @@ namespace AppDionisio.Views
         public CheckinPage()
         {
             InitializeComponent();
+            //LimparCampos();
+        }
+
+        private async void CheckinButton_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void LikeButton_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void NoLike_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void Cancelar_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private void LimparCampos()
+        {
+            LblBairro.Text = String.Empty;
+            LblCidade.Text = String.Empty;
+            LblEstado.Text = String.Empty;
+            LblLogradouro.Text = String.Empty;
+            LblNome.Text = String.Empty;
+            LblNomeCriadoEvento.Text = String.Empty;
+            LblQtdCheckins.Text = String.Empty;
+            LblQtdDesLikes.Text = String.Empty;
+            LblQtdLikes.Text = String.Empty;
         }
     }
 }
