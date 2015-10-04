78d256fba9aed77f10bbb101c1273c2bf9a1eac6
Valeska Medeiros <vahmedeiros@gmail.com>
2015-10-04 04:21:15 -0300

    Geolocalizacao

---
+++ AppDionisio/AppDionisio/AppDionisio/Models/Coordinates.cs
@@ -0,0 +1,35 @@
+﻿using Geolocator.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDionisio.Models
+{
+    class Coordinates
+    {        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+        public async Task<Coordinates> GetGeolocator()
+        {
+            Coordinates coordinates = null;
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
+
+                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+                coordinates = new Coordinates();
+                coordinates.Latitude = position.Latitude;
+                coordinates.Longitude = position.Longitude;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return coordinates;
+        }
+    }
+}
